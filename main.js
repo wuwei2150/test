81121e022b889451d92db941a6f9ba562a425bcb
wuwei <wuwei2150@163.com>
2019-03-22 23:17:38 +0800

    first commit

--- main.js
+++ main.js
@@ -1,19 +1,51 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow,contentTracing} = require('electron')
+const { ipcMain } = require('electron')
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+
+
+
+
 function createWindow () {
-  // Create the browser window.
+
+  console.log(process.version)
+  // const heapdump = require('heapdump')
+//   const nmProfiler = require('node_monitor_profiler');
+
+// nmProfiler({
+
+// project_name: '编程侠', // 你项目名称
+
+// embrace: {
+
+// tcp_host: '127.0.0.1', // Node-Monitor部署的地址
+
+// tcp_port: 30000  // Node-Monitor tcp服务的端口
+// }
+
+// })
+
+const nmProfiler = require('easy-monitor')
+ nmProfiler('test')
+// const profiler =require('v8-profiler-node8')
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true
+      
     }
   })
+
+
+
+
+
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
@@ -28,6 +60,8 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+
 }
 
 // This method will be called when Electron has finished
@@ -45,11 +79,24 @@ app.on('window-all-closed', function () {
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+
+ 
   if (mainWindow === null) {
     createWindow()
   }
+})
+
+ipcMain.on("openDir", function (e) {
+
+  console.log("openDir")
+  
+  //contentTracing.getCategories(traceCallBack)
+  // contentTracing.startRecording(options1,traceCallBack)
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+   //var fileName = dialog.showOpenDialog(mainWindow, {title: "选择一个目录", properties: ["openDirectory"]})
+   var fileName='test'
+  e.returnValue = fileName ? fileName : null
 })
 
 // In this file you can include the rest of your app's specific main process
---
+++ 新建文本文档.bat
@@ -0,0 +1,12 @@
+set npm_config_target=4.1.1
+
+set npm_config_arch=x64
+set npm_config_target_arch=x64
+
+set npm_config_disturl=https://atom.io/download/electron
+
+set npm_config_runtime=electron
+
+set npm_config_build_from_source=true
+
+pause
